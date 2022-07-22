c1f799b801662a0aea9bc9d2e6392e705f2441ad
Ferdinand MOM <ferdinand.mom@epita.fr>
2022-07-22 12:21:21 +0200

    feat(io): dump model output in binary

--- targets/main.cc
+++ targets/main.cc
@@ -8,6 +8,29 @@
 #include <torchinfer/io.hh>
 #include <torchinfer/tensor.hh>
 
+template <typename T> 
+static void display_tensor(torchinfer::Tensor<T> &out)
+{
+    std::cout << "Display value of out.data ... " << std::endl;
+
+    for (int i = 0; i < out.dims[0]; i++)
+    {
+        for (int j = 0; j < out.dims[1]; j++)
+        {
+            for (int k = 0; k < out.dims[2]; k++)
+            {
+                for (int l = 0; l < out.dims[3]; l++)
+                {
+                    std::cout << out[i * out.dims[1] * out.dims[2] * out.dims[3] + j * out.dims[2] * out.dims[3] + k * out.dims[3] + l] << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
 #if DEBUG_SPDLOG
@@ -15,7 +38,7 @@ int main(int argc, char *argv[])
 #endif
 
     argparse::ArgumentParser program("torchinfer");
-    program.add_argument("--data")
+    program.add_argument("--input")
         .help("Input data (numpy array dumped as binary)")
         .required();
     program.add_argument("--type")
@@ -23,6 +46,9 @@ int main(int argc, char *argv[])
         .required();
     program.add_argument("--onnx_ir")
         .help("ONNX Intermediate Representation (IR)")
+        .required();
+    program.add_argument("--output")
+        .help("Output model prediction")
         .required();
 
     try
@@ -37,37 +63,47 @@ int main(int argc, char *argv[])
     }
 
     // TODO: check if proper file were given to the right param.
-    auto filename_data = program.get<std::string>("--data");
+    auto filename_input = program.get<std::string>("--input");
     auto data_type = program.get<std::string>("--type");
     auto filename_onnx_ir = program.get<std::string>("--onnx_ir");
+    auto filename_output = program.get<std::string>("--output");
 
     if (data_type == "int")
     {
-        auto x = torchinfer::read_numpy_binary<int>(filename_data, data_type);
+        auto x = torchinfer::read_numpy_binary<int>(filename_input, data_type);
         auto model = torchinfer::Model<int>();
         model.load(filename_onnx_ir);
         model.summary();
-        // TODO: Find a better way to specify dims of the input tensor (not manually)
-        auto input = torchinfer::Tensor<int>(x, {3, 2, 4, 4}); 
-        model.predict(input);
+        auto input_layer = dynamic_cast<torchinfer::Inputs<int> *>(model.layers[0].get());
+        auto input = torchinfer::Tensor<int>(x, input_layer->dims);
+        auto out = model.predict(input);
+        display_tensor<int>(out);
+        torchinfer::write_numpy_binary<int>(out, filename_output);
+
     }
     else if (data_type == "float")
     {
-        auto x = torchinfer::read_numpy_binary<float>(filename_data, data_type);
+        auto x = torchinfer::read_numpy_binary<float>(filename_input, data_type);
         auto model = torchinfer::Model<float>();
         model.load(filename_onnx_ir);
         model.summary();
-        auto input = torchinfer::Tensor<float>(x, {3, 2, 4, 4}); 
-        model.predict(input);
+        auto input_layer = dynamic_cast<torchinfer::Inputs<float> *>(model.layers[0].get());
+        auto input = torchinfer::Tensor<float>(x, input_layer->dims);
+        auto out = model.predict(input);
+        display_tensor<float>(out);
+        torchinfer::write_numpy_binary<float>(out, filename_output);
     }
     else if (data_type == "double")
     {
-        auto x = torchinfer::read_numpy_binary<double>(filename_data, data_type);
+        auto x = torchinfer::read_numpy_binary<double>(filename_input, data_type);
         auto model = torchinfer::Model<double>();
         model.load(filename_onnx_ir);
         model.summary();
-        auto input = torchinfer::Tensor<double>(x, {3, 2, 4, 4}); 
-        model.predict(input);
+        auto input_layer = dynamic_cast<torchinfer::Inputs<double> *>(model.layers[0].get());
+        auto input = torchinfer::Tensor<double>(x, input_layer->dims);
+        auto out = model.predict(input);
+        display_tensor<double>(out);
+        torchinfer::write_numpy_binary<double>(out, filename_output);
     }
     else
         throw std::runtime_error("main: Unknown data type");
--- torchinfer/io.hh
+++ torchinfer/io.hh
@@ -13,25 +13,35 @@ namespace torchinfer
     std::vector<T> read_numpy_binary(const std::string &filename, const std::string &data_type)
     {
         /*
-        Read from numpy write_bin().
-
-        file format:
-            - n (int)
-            - c (int)
-            - h (int)
-            - w (int)
-            - format [int, float, double] (byte)
-            - data
-        */
+         * Reads a numpy binary file (dumped with write_bin()) and returns a vector of type T.
+         *
+         * File format:
+         *    - n (int)
+         *    - c (int)
+         *    - h (int)
+         *    - w (int)
+         *    - format [int, float, double] (byte)
+         *    - data
+         *
+         * Parameters:
+         *   filename: path to the numpy binary file
+         *   data_type: data type of the numpy binary file
+         *
+         * Returns:
+         *   A vector of type T.
+         */
 
         std::map<std::string, char> datatype_to_format = {
             {"int", 'i'},
             {"float", 'f'},
-            {"double", 'd'}
-        };
+            {"double", 'd'}};
 
         std::ifstream file(filename, std::ios::binary);
 
+        if (!file.is_open())
+            throw std::runtime_error("read_numpy_binary: file not opened");
+
+        // Dimensions
         int n = -1, c = -1, h = -1, w = -1;
         file.read(reinterpret_cast<char *>(&n), sizeof(int));
         file.read(reinterpret_cast<char *>(&c), sizeof(int));
@@ -41,6 +51,7 @@ namespace torchinfer
         if (n == -1 || c == -1 || h == -1 || w == -1)
             throw std::runtime_error("read_numpy_binary: No dimensions (n,c,h,w) dumped in binary");
 
+        // Format
         char format = '\0';
         file.read(reinterpret_cast<char *>(&format), sizeof(char));
 
@@ -49,13 +60,72 @@ namespace torchinfer
 
         if (datatype_to_format[data_type] != format)
             throw std::runtime_error("read_numpy_binary: Specified data type does not match dumped data type");
-
+        
         std::vector<T> vec(n * c * h * w);
         file.read(reinterpret_cast<char *>(vec.data()), n * c * h * w * sizeof(T));
         return vec;
     }
 
     template <typename T>
+    void write_numpy_binary(Tensor<T> &tensor, const std::string &filename)
+    {
+        /*
+            * Writes a tensor following numpy binary format.
+            *
+            * File format:
+            *    - n (int)
+            *    - c (int)
+            *    - h (int)
+            *    - w (int)
+            *    - format [int, float, double] (byte)
+            *    - data
+            *
+            * Parameters:
+            *   tensor: tensor to be dumped
+            *   filename: path to the numpy binary file
+        */ 
+
+        std::ofstream file(filename, std::ios::binary);
+        
+        if (!file.is_open())
+            throw std::runtime_error("write_numpy_binary: file not opened");
+
+        // Dimensions
+        int n = tensor.dims[0];
+        int c = tensor.dims[1];
+        int h = tensor.dims[2];
+        int w = tensor.dims[3];
+
+        file.write(reinterpret_cast<char *>(&n), sizeof(int));
+        file.write(reinterpret_cast<char *>(&c), sizeof(int));
+        file.write(reinterpret_cast<char *>(&h), sizeof(int));
+        file.write(reinterpret_cast<char *>(&w), sizeof(int));
+
+        // format
+        if (std::is_same<T, int>::value) {
+            char format = 'i';
+            file.write(reinterpret_cast<char *>(&format), sizeof(char));
+        }
+        else if (std::is_same<T, float>::value)
+        {
+            char format = 'f';
+            file.write(reinterpret_cast<char *>(&format), sizeof(char));
+        }
+        else if (std::is_same<T, double>::value) {
+            char format = 'd';
+            file.write(reinterpret_cast<char *>(&format), sizeof(char));
+        }
+        else
+            throw std::runtime_error("write_numpy_binary: Unsupported data type");
+
+        // data
+        file.write(reinterpret_cast<char *>(tensor.data.data()), n * c * h * w * sizeof(T));
+
+        file.close();
+    }
+
+    template <typename T>
+    
     T read_scalar_from_stream(std::ifstream &file, std::string param_name)
     {
         T param = -1;
@@ -74,7 +144,7 @@ namespace torchinfer
             throw std::runtime_error("read_vector_from_stream: No " + param_name + " dumped in binary");
         return param;
     }
-    
+
     template <typename T>
     std::vector<T> read_raw_data_from_stream(std::ifstream &file, std::vector<int> &dims, std::string param_name)
     {
